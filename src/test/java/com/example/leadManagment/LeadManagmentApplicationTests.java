32ef350a8c394fdb547ba6a330a7579f97ec83c7
Ruqhiya. <ummehani01072002@gmail.com>
2026-07-10 13:03:49 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/example/leadManagment/LeadManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leadManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
